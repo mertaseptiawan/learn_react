772072a041461f49e0d4197643c5f751587b9c0a
mertaseptiawan <mertaseptiawan@gmail.com>
2022-12-13 12:03:12 +0800

    git init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Employee from './component/Employee';
 
 function App() {
+  const showEmployees = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showEmployees ? (
+        <div>
+          <Employee name="merta" />
+          <Employee />
+          <Employee />
+          <Employee />
+        </div>
+      ) : (
+        <p>you canot see the employees</p>
+      )}
     </div>
   );
 }
---
+++ src/component/Employee.js
@@ -0,0 +1,5 @@
+function Employee(props) {
+    return <h3>Here is an Employee {props.name}</h3>;
+}
+
+export default Employee;
